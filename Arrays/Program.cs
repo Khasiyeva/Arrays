40398393e382588812613bbbd1a0acf5d4520006
Khasiyeva <bd2q9mvwb@code.edu.az>
2023-09-29 11:39:15 +0400

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,62 @@
+﻿//Task1
+//int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+//for (int i = 0; i < numbers.Length; i++)
+//{
+// for(int j=1;j<6;j++)
+//    {
+//        if (numbers[i] == j * j)
+//        {
+//            Console.WriteLine(numbers[i]);
+//        }
+//    }
+//}
+
+
+
+//Task2
+//int[] numbers = { 2, 5, 6, 8, 19, 25, 22, 14 };
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    if (numbers[i] % 2 == 1)
+//    {
+//        Console.WriteLine(numbers[i]);
+//    }
+//}
+
+
+//Task3
+//int[] numbers = { 2, 5, 6, 8, 1 };
+//int sum = 0;
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        sum = sum + numbers[i];
+
+//    }
+
+//}
+//Console.WriteLine(sum);
+
+
+
+//Task4
+//string word = "dad";
+//string word2 = "";
+//for (int i = word.Length - 1; i >= 0; i--)
+//{
+//    word2 += word[i];
+//}
+//if (word2==word)
+//{
+//    Console.WriteLine("YES");
+//}
+//else
+//{
+//    Console.WriteLine("NO");
+//}
+    
+
+
+
+
